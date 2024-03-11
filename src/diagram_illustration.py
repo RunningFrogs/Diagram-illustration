a82d86804edbcfcda97c59e80c62a5dec581ed7e
billy <info.runningfrogs@gmail.com>
2024-03-11 16:36:47 +0100

    Combine all diagrams to one

--- src/diagram_illustration.py
+++ src/diagram_illustration.py
@@ -2,35 +2,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
+# Erzeugen von zufälligen Daten für Boxplot mit Ausreißern
 data_boxplot = [np.random.normal(0, std, 100) for std in range(1, 4)]
+# Hinzufügen von Ausreißern
 outliers = np.random.uniform(low=-9, high=9, size=(10, 3))
 data_boxplot_with_outliers = [np.concatenate([data, outlier], axis=0) for data, outlier in zip(data_boxplot, outliers.T)]
 
+# Erzeugen von zufälligen Daten für die Heatmap
 data_heatmap = np.random.rand(10, 10)
 
+# Erzeugen von zufälligen Daten für das gestapelte Balkendiagramm
 category_1 = np.random.rand(10)
 category_2 = np.random.rand(10)
 
-plt.figure(figsize=(5, 5))
+# Boxplot mit Ausreißern
+plt.figure(figsize=(15, 5))
+plt.subplot(1, 3, 1)
 plt.boxplot(data_boxplot_with_outliers, patch_artist=True)
 plt.title('Boxplot')
-boxplot_filename = '../diagrams/boxplot.png'
-plt.savefig(boxplot_filename)
-plt.close()
 
-plt.figure(figsize=(8, 6))
+# Heatmap
+plt.subplot(1, 3, 2)
 sns.heatmap(data_heatmap, annot=True, cmap='coolwarm')
 plt.title('Heatmap')
-heatmap_filename = '../diagrams/heatmap.png'
-plt.savefig(heatmap_filename)
-plt.close()
 
-plt.figure(figsize=(10, 5))
-ind = np.arange(len(category_1))
-width = 0.35
-plt.bar(ind, category_1, width, label='Kategorie 1', color='b')
-plt.bar(ind, category_2, width, bottom=category_1, label='Kategorie 2', color='r')
-plt.title('Histogramm')
-barchart_filename = '../diagrams/histogram.png'
-plt.savefig(barchart_filename)
-plt.close()
+# Gestapeltes Balkendiagramm
+ind = np.arange(len(category_1))  # die x-Locations für die Gruppen
+width = 0.35  # die Breite der Balken
+
+plt.subplot(1, 3, 3)
+p1 = plt.bar(ind, category_1, width, color='b')
+p2 = plt.bar(ind, category_2, width, bottom=category_1, color='r')
+plt.title('Gestapeltes Säulendiagramm')
+
+plt.tight_layout()
+plt.savefig('../diagrams/diagrams.png')
